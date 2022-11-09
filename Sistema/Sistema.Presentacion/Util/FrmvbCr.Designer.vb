cdcb58bfc45cde354b987a406bb17b2aa03c00d4
Raul Armas <you@example.com>
2022-11-09 09:19:10 -0500

    updates parse lists

--- Sistema/Sistema.Presentacion/Util/FrmvbCr.Designer.vb
+++ Sistema/Sistema.Presentacion/Util/FrmvbCr.Designer.vb
@@ -24,30 +24,84 @@ Partial Class FrmvbCr
     Private Sub InitializeComponent()
         Me.MyRichTextControl = New System.Windows.Forms.RichTextBox()
         Me.btnJustifyText = New System.Windows.Forms.Button()
+        Me.btnRetrieveLists = New System.Windows.Forms.Button()
+        Me.RichResultTextBox = New System.Windows.Forms.RichTextBox()
+        Me.btnLimpiar = New System.Windows.Forms.Button()
+        Me.btnRetrieveLists2 = New System.Windows.Forms.Button()
+        Me.btnRetrieveLists3 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'MyRichTextControl
         '
         Me.MyRichTextControl.Location = New System.Drawing.Point(68, 49)
         Me.MyRichTextControl.Name = "MyRichTextControl"
-        Me.MyRichTextControl.Size = New System.Drawing.Size(1025, 293)
+        Me.MyRichTextControl.Size = New System.Drawing.Size(1047, 180)
         Me.MyRichTextControl.TabIndex = 1
         Me.MyRichTextControl.Text = ""
         '
         'btnJustifyText
         '
-        Me.btnJustifyText.Location = New System.Drawing.Point(1018, 369)
+        Me.btnJustifyText.Location = New System.Drawing.Point(1121, 49)
         Me.btnJustifyText.Name = "btnJustifyText"
         Me.btnJustifyText.Size = New System.Drawing.Size(75, 23)
         Me.btnJustifyText.TabIndex = 2
         Me.btnJustifyText.Text = "Justify Text"
         Me.btnJustifyText.UseVisualStyleBackColor = True
         '
+        'btnRetrieveLists
+        '
+        Me.btnRetrieveLists.Location = New System.Drawing.Point(1121, 78)
+        Me.btnRetrieveLists.Name = "btnRetrieveLists"
+        Me.btnRetrieveLists.Size = New System.Drawing.Size(75, 23)
+        Me.btnRetrieveLists.TabIndex = 3
+        Me.btnRetrieveLists.Text = "Leer Listas"
+        Me.btnRetrieveLists.UseVisualStyleBackColor = True
+        '
+        'RichResultTextBox
+        '
+        Me.RichResultTextBox.Location = New System.Drawing.Point(68, 246)
+        Me.RichResultTextBox.Name = "RichResultTextBox"
+        Me.RichResultTextBox.Size = New System.Drawing.Size(1047, 180)
+        Me.RichResultTextBox.TabIndex = 4
+        Me.RichResultTextBox.Text = ""
+        '
+        'btnLimpiar
+        '
+        Me.btnLimpiar.Location = New System.Drawing.Point(1121, 107)
+        Me.btnLimpiar.Name = "btnLimpiar"
+        Me.btnLimpiar.Size = New System.Drawing.Size(75, 23)
+        Me.btnLimpiar.TabIndex = 5
+        Me.btnLimpiar.Text = "Limpiar"
+        Me.btnLimpiar.UseVisualStyleBackColor = True
+        '
+        'btnRetrieveLists2
+        '
+        Me.btnRetrieveLists2.Location = New System.Drawing.Point(1202, 78)
+        Me.btnRetrieveLists2.Name = "btnRetrieveLists2"
+        Me.btnRetrieveLists2.Size = New System.Drawing.Size(75, 23)
+        Me.btnRetrieveLists2.TabIndex = 6
+        Me.btnRetrieveLists2.Text = "Leer Listas 2"
+        Me.btnRetrieveLists2.UseVisualStyleBackColor = True
+        '
+        'btnRetrieveLists3
+        '
+        Me.btnRetrieveLists3.Location = New System.Drawing.Point(1283, 78)
+        Me.btnRetrieveLists3.Name = "btnRetrieveLists3"
+        Me.btnRetrieveLists3.Size = New System.Drawing.Size(75, 23)
+        Me.btnRetrieveLists3.TabIndex = 7
+        Me.btnRetrieveLists3.Text = "Leer Listas 3"
+        Me.btnRetrieveLists3.UseVisualStyleBackColor = True
+        '
         'FrmvbCr
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1157, 437)
+        Me.ClientSize = New System.Drawing.Size(1413, 527)
+        Me.Controls.Add(Me.btnRetrieveLists3)
+        Me.Controls.Add(Me.btnRetrieveLists2)
+        Me.Controls.Add(Me.btnLimpiar)
+        Me.Controls.Add(Me.RichResultTextBox)
+        Me.Controls.Add(Me.btnRetrieveLists)
         Me.Controls.Add(Me.btnJustifyText)
         Me.Controls.Add(Me.MyRichTextControl)
         Me.Name = "FrmvbCr"
@@ -58,4 +112,9 @@ Partial Class FrmvbCr
 
     Friend WithEvents MyRichTextControl As RichTextBox
     Friend WithEvents btnJustifyText As Button
+    Friend WithEvents btnRetrieveLists As Button
+    Friend WithEvents RichResultTextBox As RichTextBox
+    Friend WithEvents btnLimpiar As Button
+    Friend WithEvents btnRetrieveLists2 As Button
+    Friend WithEvents btnRetrieveLists3 As Button
 End Class
--- Sistema/Sistema.Presentacion/Util/FrmvbCr.vb
+++ Sistema/Sistema.Presentacion/Util/FrmvbCr.vb
@@ -1,7 +1,110 @@
-﻿Public Class FrmvbCr
+﻿Imports System.Text.RegularExpressions
+
+Public Class FrmvbCr
     Private Sub btnJustifyText_Click(sender As Object, e As EventArgs) Handles btnJustifyText.Click
         MyRichTextControl.Text = JustifyText(MyRichTextControl.Text)
     End Sub
+
+
+    Private Function ParseList(ByRef Text As String) As String
+        Dim matches, matches2 As MatchCollection
+        Dim text2 As String = String.Empty
+        Dim text3 As String = String.Empty
+        Dim text4 As String = String.Empty
+        Dim text5 As String = String.Empty
+        matches = Regex.Matches(Text, "<li>(.*?)</li>", RegexOptions.IgnoreCase)
+        If matches.Count > 0 Then
+            For Each item As Match In matches
+                If item.Value.Contains("<ul>") Then
+                    text4 = Regex.Matches(item.Value.ToString(), "<li>(.*?)<ul>", RegexOptions.IgnoreCase)(0).Value.ToString()
+                    text2 &= text4
+                    text2 = text2.Replace("<li>", vbCrLf & " -")
+                    text2 = text2.Replace("<ul>", "")
+                    'Text = Replace(Text, Regex.Matches(item.Value.ToString(), "<li>(.*?)<ul>", RegexOptions.IgnoreCase)(0).Value.ToString(), vbCrLf & "-" & text4)
+                    matches2 = Regex.Matches(item.Value.ToString(), "<ul>(.*?)</li>", RegexOptions.IgnoreCase)
+                    If matches2.Count > 0 Then
+                        text3 = ParseList(matches2(0).Value.ToString())
+                        text2 &= " " & text3
+                        text2 = text2.Replace("<ul>", "")
+                        text2 = text2.Replace("<li>", "   -")
+                        text2 = text2.Replace("</li>", "")
+                        text3 = text3.Replace("</li>", "")
+                        ' Text = Replace(Text, matches2(0).Value, "   -" & text3)
+                    End If
+                ElseIf item.Value.contains("</ul>") Then
+                    text2 = text2.Replace("</li>", "")
+                    text2 = text2.Replace("</li>", "")
+                    text2 = text2.Replace("<ul>", vbCrLf & "   -")
+                Else
+                    text2 &= " " & item.Value.Replace("<li>", vbCrLf & "   -")
+                    text2 = text2.Replace("</li>", "")
+                    text5 = text5.Replace("</li>", "")
+                    'Text = Replace(Text, item.Value, item.Value.Replace("<li>", vbCrLf & "   -"))
+                End If
+            Next
+        End If
+        Return text2
+    End Function
+
+
+    Private Function ParseList3(Text As String) As String
+        Dim matches, matches2 As MatchCollection
+        Dim text2 As String = String.Empty
+
+        matches = Regex.Matches(Text, "<li>(.*?)</li>", RegexOptions.IgnoreCase)
+        If matches.Count > 0 Then
+            For Each item As Match In matches
+                If item.Value.Contains("<ul>") Then
+                    text2 &= Regex.Matches(item.Value.ToString(), "<li>(.*?)<ul>", RegexOptions.IgnoreCase)(0).Value.ToString()
+                    text2 = text2.Replace("<li>", vbCrLf & "-")
+                    text2 = text2.Replace("<ul>", "")
+                    matches2 = Regex.Matches(item.Value.ToString(), "<ul>(.*?)</li>", RegexOptions.IgnoreCase)
+                    If matches2.Count > 0 Then
+                        text2 &= " " & ParseList(matches2(0).Value.ToString())
+                        text2 = text2.Replace("<ul>", "")
+                        text2 = text2.Replace("<li>", "   -")
+                        text2 = text2.Replace("</li>", "")
+                    End If
+                Else
+                    text2 &= " " & item.Value.Replace("<li>", vbCrLf & "   -")
+                    text2 = text2.Replace("</li>", "")
+                End If
+            Next
+        End If
+        Return text2
+    End Function
+
+    Private Function ParseList2(ByRef Text As String) As String
+        Dim matches, matches2 As MatchCollection
+        Dim text2 As String = String.Empty
+
+        matches = Regex.Matches(Text, "<ul>(.*?)</ul>", RegexOptions.IgnoreCase)
+        If matches.Count > 0 Then
+            For Each item As Match In matches
+                If item.Value.Contains("<ul>") Then
+                    text2 &= Regex.Matches(item.Value.ToString(), "<li>(.*?)<ul>", RegexOptions.IgnoreCase)(0).Value.ToString()
+                    text2 = text2.Replace("<li>", vbCrLf & "-")
+                    text2 = text2.Replace("<ul>", "")
+                    Replace(Text, Regex.Matches(item.Value.ToString(), "<li>(.*?)<ul>", RegexOptions.IgnoreCase)(0).Value.ToString(), text2)
+                    matches2 = Regex.Matches(item.Value.ToString(), "<ul>(.*?)</li>", RegexOptions.IgnoreCase)
+                    If matches2.Count > 0 Then
+                        text2 &= " " & ParseList(matches2(0).Value.ToString())
+                        text2 = text2.Replace("<ul>", "")
+                        text2 = text2.Replace("<li>", "   -")
+                        text2 = text2.Replace("</li>", "")
+                        Replace(Text, matches2(0).Value, text2)
+                    End If
+                Else
+                    text2 &= " " & item.Value.Replace("<li>", vbCrLf & "   -")
+                    text2 = text2.Replace("</li>", "")
+                    Replace(Text, item.Value, text2)
+                End If
+            Next
+        End If
+        Return Text
+    End Function
+
+
 
     Private Function JustifyText(ByVal Text As String) As String
         Dim s As String = Text
@@ -52,4 +155,39 @@
         End If
         Return s
     End Function
+
+    Private Sub btnLimpiar_Click(sender As Object, e As EventArgs) Handles btnLimpiar.Click
+        'MyRichTextControl.Text = String.Empty
+        RichResultTextBox.Text = String.Empty
+    End Sub
+
+    Private Sub btnRetrieveLists_Click(sender As Object, e As EventArgs) Handles btnRetrieveLists.Click
+        If MyRichTextControl.Text <> String.Empty Then
+            RichResultTextBox.Text = ParseList(MyRichTextControl.Text)
+        End If
+    End Sub
+
+    Private Sub btnRetrieveLists2_Click(sender As Object, e As EventArgs) Handles btnRetrieveLists2.Click
+        If MyRichTextControl.Text <> String.Empty Then
+            ParseList2(MyRichTextControl.Text)
+            RichResultTextBox.Text = MyRichTextControl.Text
+        End If
+    End Sub
+
+    Private Sub btnRetrieveLists3_Click(sender As Object, e As EventArgs) Handles btnRetrieveLists3.Click
+
+        Dim matches1 As MatchCollection
+        Dim cont As String = MyRichTextControl.Text
+        cont = Replace(cont, vbLf, "")
+        cont = Replace(cont, vbTab, "")
+        matches1 = Regex.Matches(cont, "<ul>(.*?)</ul>", RegexOptions.IgnoreCase)
+        For Each item As Match In matches1
+            If Not String.IsNullOrEmpty(ParseList(item.Value.ToString())) Then
+                cont = Replace(cont, item.Value.ToString(), ParseList(item.Value.ToString()))
+            End If
+        Next
+        If cont <> String.Empty Then
+            RichResultTextBox.Text = cont 'ParseList3(MyRichTextControl.Text)
+        End If
+    End Sub
 End Class
